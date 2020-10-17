616e812ababdcdaefe7270ff6bcc2a9d7bdbecb7
Ailine <ailine.soto@outlook.fr>
2020-10-18 01:29:32 +0200

    config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    devServer: {
-      proxy: "http://localhost:8000/",
-      host: "localhost",
-      publicPath: "http://localhost:8080/" // utile pour supprimer l'erreur sockjs-node (Not Found) dans la console du navigateur
-    },
-  };
+  devServer: {
+    proxy: process.env.VUE_APP_BACKEND_URL,
+    host: process.env.VUE_APP_HOST,
+    publicPath: process.env.VUE_APP_FRONTEND_URL === 'production' // utile pour supprimer l'erreur sockjs-node (Not Found) dans la console du navigateur
+  },
+};
